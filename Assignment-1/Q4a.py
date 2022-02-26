f77d62025a3b6aad0758cc2bd7615d2478d6e896
r79007 <rohan20537@iiitd.ac.in>
2022-02-26 18:51:32 +0530

    initial-commit

---
+++ Assignment-1/Q4a.py
@@ -0,0 +1,118 @@
+match=2
+mismatch=-1
+gap=-2
+
+seq2 = "ATCAGAGTA"
+seq1= "TTCAGTA"
+
+
+def initialize_matrix():
+    score=0
+    
+    mat= [[0 for i in range(len(seq2)+1)] for j in range(len(seq1) + 1)]
+
+    for i in range (0,len(seq1)+1):
+        mat[i][0]=score
+        score+=gap
+    
+    score=0
+    for i in range (0,len(seq2)+1):
+        mat[0][i]=score
+        score+=gap
+
+
+
+    for i in range(1, len(seq1)+1):
+        for j in range(1,len(seq2)+1):
+            if(seq1[i-1]==seq2[j-1]):
+                mat[i][j]=match+mat[i-1][j-1]
+            else:
+                mat[i][j]=max(mat[i-1][j]+gap, mat[i][j-1]+gap, mat[i-1][j-1]+mismatch)
+            
+
+    return mat
+
+
+def print_matrix(A):
+
+    for i in range (0,len(seq1)+1):
+        for j in range(0, len(seq2)+1):
+            print(A[i][j], end=" ")
+
+        print()
+
+opt1=[]
+opt2=[]
+scores=[]
+
+def findAllOptAlign(seq1, seq2, i, j, newSeq1, newSeq2, currScore, arr):
+
+    if((i==0 and j==0) or (i<0 or j<0)):
+        opt1.append(newSeq1)
+        opt2.append(newSeq2)
+        scores.append(currScore)
+        return
+    
+    if(i==0):
+        findAllOptAlign(seq1, seq2, i, j-1, newSeq1+"-", newSeq2+seq2[j-1], currScore+gap, arr)
+        return
+    if(j==0):
+
+        findAllOptAlign(seq1, seq2, i-1, j, newSeq1+seq1[i-1], newSeq2+"-", currScore+gap, arr)
+        return
+    
+    #now we try to find the optimal alignments from all three possiblities -> match, mismatch, or gap
+
+    if(seq1[i-1]==seq2[j-1]):
+        findAllOptAlign(seq1, seq2,i-1,j-1,newSeq1+seq1[i-1], newSeq2+seq2[j-1], currScore+match, arr)
+        if(arr[i][j]-gap==arr[i-1][j]):
+            findAllOptAlign(seq1, seq2, i-1, j, newSeq1+seq1[i-1], newSeq2+"-", currScore+gap, arr)
+        if(arr[i][j]-gap==arr[i][j-1]):
+            findAllOptAlign(seq1, seq2, i, j-1, newSeq1+"-", newSeq2+seq2[j-1], currScore+gap, arr)
+
+    else:
+        findAllOptAlign(seq1, seq2,i-1,j-1,newSeq1+seq1[i-1],newSeq2+seq2[j-1],currScore+mismatch, arr)
+        if(arr[i][j]-gap==arr[i-1][j]):
+            findAllOptAlign(seq1, seq2, i-1, j, newSeq1+seq1[i-1], newSeq2+"-", currScore+gap, arr)
+        if(arr[i][j]-gap==arr[i][j-1]):
+            findAllOptAlign(seq1, seq2, i, j-1, newSeq1+"-", newSeq2+seq2[j-1], currScore+gap, arr)
+
+
+
+def print_opt_alignments(seq1, seq2, arr):
+
+
+    findAllOptAlign(seq1,seq2, len(seq1), len(seq2), "", "", 0,arr)
+
+    idx=-1
+    maxScore=float('-inf')
+
+
+    for i in scores:
+        if(i>maxScore):
+            maxScore=i
+
+    for j in range(len(opt1)):
+        if(scores[j]==maxScore):
+            print(opt1[j][::-1])
+            print(opt2[j][::-1])
+            print('Score: ',scores[j])
+            print('-------------------------------')
+
+
+
+print("Yes the results will change")
+arr=initialize_matrix()
+print("The bi-dimensional array is \n")
+
+print_matrix(arr)
+print('\n')
+print("There are more than 1 optimal alignments possible\n")
+print_opt_alignments(seq1, seq2, arr)
+
+
+
+
+
+
+
