50566e818902c5af40c1d94eb0befea73288e55d
r79007 <rohan20537@iiitd.ac.in>
2022-04-20 13:10:42 +0530

    2nd-almost done

--- Assignment-2/q1.py
+++ Assignment-2/q1.py
@@ -24,15 +24,18 @@ for i in range(len(protein_seq)):
 #For helix
 
 i=0
+#"We check all possible stretches of length 6
 while(i<len(protein_seq)-5):
-    #Counting Favourable residues
+    #Counting Favourable residues in these stretches
     num_fav=lst[0]
+    #Checking in stretches of size 6
     for j in range(lst[2]):
         residue=protein_seq[i+j]
+        #If P(H)>=1 then we increment num_fav(favourable residues)
         if(alpha_dict[residue]>=1):
             num_fav+=lst[1]
 
-    #
+    #If then number of favourable residues is >=4 then this site can have a helix
 
     if(num_fav>=4):
         for j in range(lst[2]):
@@ -40,31 +43,35 @@ while(i<len(protein_seq)-5):
 
         #Now we do extensions
 
+        #We first try to extend in stretches of 4 to the right
         k=i+3
         while(k-3>=0):
             score=0
             for j in range(0,4):
                 residue=protein_seq[k-j]
                 score+=alpha_dict[residue]
-
+            #If the current right stretch has sum of the P(H) of the residues >=4 then this can be a helix
             if(score>=4):
                 for j in range(0,4):
                     helix[k-j]="H"
                 k-=1
+
+            #If not then we break
             else:
                 break
-
+        #Now we try to extend in stretches of 4 to the left
         k = i+2
         while (k+4 <= len(protein_seq)):
             score = 0
             for j in range(0, 4):
                 residue = protein_seq[k + j]
                 score += alpha_dict[residue]
-
+            # If the current right stretch has sum of the P(H) of the residues >=4 then this can be a helix
             if (score >= 4):
                 for j in range(0, 4):
                     helix[k + j] = "H"
                 k += 1
+            #Else we break
             else:
                 break
 
@@ -77,48 +84,56 @@ while(i<len(protein_seq)-5):
 
 i=0
 
+#We check all possible stretches of length 5
+
 while(i<len(protein_seq)-4):
 
-    # Counting Favourable residues
+    # Counting Favourable residues in these stretches
     num_fav = lst[0]
+    # Checking in stretches of size 5
     for j in range(lst[2]-1):
         residue = protein_seq[i + j]
+        # If P(H)>=1 then we increment num_fav(favourable residues)
         if (beta_dict[residue] >= 1):
             num_fav += lst[1]
 
-    #
 
+    #If number of favourable residues is >= 3 then this site can be a strand
     if (num_fav >= 3):
         for j in range(lst[2]-1):
             strand[i + j] = "S"
 
         # Now we do extensions
 
+        # We first try to extend in stretches of 3 to the right
         k = i + 2
         while (k - 3 >= 0):
             score = 0
             for j in range(0, 4):
                 residue = protein_seq[k - j]
                 score += beta_dict[residue]
-
+            # If sum of P(S) of the residues in this stretch is > 4 then this site can be a strand
             if (score > 4):
                 for j in range(0, 4):
                     strand[k - j] = "S"
                 k -= 1
+            #Else we break from working on this stretch
             else:
                 break
 
+        # Now we try to extend in stretches of 3 to the left
         k = i + 2
         while (k + 4 <= len(protein_seq)):
             score = 0
             for j in range(0, 4):
                 residue = protein_seq[k + j]
                 score += beta_dict[residue]
-
+            # If sum of P(S) of the residues in this stretch is > 4 then this site can be a strand
             if (score > 4):
                 for j in range(0, 4):
                     strand[k + j] = "S"
                 k += 1
+            # Else we break from working on this stretch
             else:
                 break
 
@@ -126,27 +141,28 @@ while(i<len(protein_seq)-4):
 
 i=0
 
-
+# Now we fill in the predictions for generating the final result
 
 while(i<len(protein_seq)):
 
     idx=0
-
+    #If our method could not predict whether it could be a strand or a helix, we take that to be a turn denoted by T
     if(helix[i]=='#' and strand[i]=='#'):
         result[i]='T'
         i+=1
-
+    #If it is predicted as Helix but not as Strand then it could be a helix
     elif(helix[i]=='H' and strand[i]=='#'):
         result[i]='H'
         i+=1
-
+    # If it is predicted as Strand but not as Helix then it could be a Strand
     elif(helix[i]=='#' and strand[i]=='S'):
         result[i]='S'
         i+=1
-
+    #If it is predicted both as a strand and a helix then we check whose score is higher, the one with the higher score(sum of probabilities) has more probability to be a correct prediction
     else:
 
         while(idx+i<len(protein_seq)):
+            #We find the limit(last index in the stretch) to which the predictions are both H and S
             if(helix[i+idx]=='H' and strand[i+idx]=='S'):
                 idx+=1
             else:
@@ -156,14 +172,16 @@ while(i<len(protein_seq)):
         strand_score = 0
 
         for j in range(0, idx):
+            # Now we calculate the score of helix and strand till this index
             residue_alpha=protein_seq[i+j]
             residue_beta = protein_seq[i + j]
             hel_score+=alpha_dict[residue_alpha]
             strand_score+=beta_dict[residue_beta]
-
+        #If strand has highes score then it has more probability to be correct
         if(strand_score>hel_score):
             for j in range(0, idx):
                 result[i+j]='S'
+        #Else helix has more probabilty to be correct
         else:
             for j in range(0, idx):
                 result[i + j] = 'H'
@@ -172,12 +190,14 @@ while(i<len(protein_seq)):
 
 server_result="TTTT     HHHHHH EEEEEETTEEEEEEEETTEEEEEGGGG  HHHHH   HHHHHHH  GGG EEEETTEEE EEEEEEETTEEEEEE   TTTT        TTTEEEEEEEEETTEEEEEEEEEETTTT B    TTTTTTTEE "
 result=''.join(result)
+
+print("The following sequences are the original protein sequence, the result that we got from the Chau-Fasman Method and the result from the server respectively\n")
+
 print(protein_seq)
 print(result)
 print(server_result)
 
 print()
-
 print("Differing Regions(The first string represents the server result, and the second string represents my result): ")
 print("Here the gaps in the server result have been substituted by C, because the gaps actually mean coils")
 
